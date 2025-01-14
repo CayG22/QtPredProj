5af058e8e9abca39b73185fc21e6d338049ec83a
CayG22 <136837724+CayG22@users.noreply.github.com>
2025-01-14 13:52:43 -0600

    Added Crest Buffs to CrestPage

--- crestpage.cpp
+++ crestpage.cpp
@@ -10,10 +10,57 @@ CrestPage::CrestPage(Crest crest, QWidget *parent)
     qDebug() << image_path;
     QPixmap pixmap(image_path);
     ui->CrestImage->setPixmap(pixmap);
-
+    fillTable();
 }
 
 CrestPage::~CrestPage()
 {
     delete ui;
+}
+
+void CrestPage::fillTable(){
+    ui->BuffTable->setColumnCount(2);
+    ui->BuffTable->setHorizontalHeaderLabels({"Buff","Value"});
+
+    QList<QPair<QString,QVariant>> buffs = {
+        {"Physical Power", currentCrest.m_physical_power},
+        {"Magical Power", currentCrest.m_magical_power},
+        {"Physical Penetration", currentCrest.m_physical_penetration},
+        {"Critical Chance", currentCrest.m_critical_chance},
+        {"Attack Speed", currentCrest.m_attack_speed},
+        {"Lifesteal", currentCrest.m_lifesteal},
+        {"Omnivamp", currentCrest.m_omnivmap},
+        {"Physical Armor", currentCrest.m_physical_armor},
+        {"Magical Armor", currentCrest.m_magical_armor},
+        {"Tenacity", currentCrest.m_tenacity},
+        {"Max Health: ", currentCrest.m_max_health},
+        {"Max Mana: ", currentCrest.m_max_mana},
+        {"Health Regeneration: ", currentCrest.m_health_regeneration},
+        {"Ability Haste: ", currentCrest.m_ability_haste},
+        {"Movement Speed: ", currentCrest.m_movement_speed}
+    };
+
+    int validRowCount = 0;
+    for(const auto& buff:buffs){
+        if(!buff.second.isNull() && buff.second.toDouble() != 0){
+            validRowCount++;
+        }
+    }
+    ui->BuffTable->setRowCount(validRowCount);
+
+    int row = 0;
+    for(const auto& buff : buffs){
+        if(buff.second.isNull() || buff.second.toDouble() == 0){
+            continue; //Skip if empty or zero value buffs
+        }
+        ui->BuffTable->setItem(row,0,new QTableWidgetItem(buff.first));
+        ui->BuffTable->setItem(row,1,new QTableWidgetItem(buff.second.toString()));
+        row++;
+    }
+
+    ui->BuffTable->resizeColumnsToContents();
+    ui->BuffTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+    ui->BuffTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+
+
 }
