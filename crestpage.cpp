fffdad8ae08390ae731794ccdc4c1128b452a121
CayG22 <136837724+CayG22@users.noreply.github.com>
2025-01-15 12:02:43 -0600

    Added CrestPage UI improvements

--- crestpage.cpp
+++ crestpage.cpp
@@ -10,6 +10,11 @@ CrestPage::CrestPage(Crest crest, QWidget *parent)
     qDebug() << image_path;
     QPixmap pixmap(image_path);
     ui->CrestImage->setPixmap(pixmap);
+    ui->CrestImage->setAlignment(Qt::AlignCenter);
+    ui->CrestName->setText(currentCrest.m_name);
+    ui->CrestName->setAlignment(Qt::AlignCenter);
+    ui->CrestName->setStyleSheet("QLabel{color:#6763A0;font-size:30px}");
+    this->setStyleSheet("background-color:#191923");
     fillTable();
 }
 
@@ -33,11 +38,11 @@ void CrestPage::fillTable(){
         {"Physical Armor", currentCrest.m_physical_armor},
         {"Magical Armor", currentCrest.m_magical_armor},
         {"Tenacity", currentCrest.m_tenacity},
-        {"Max Health: ", currentCrest.m_max_health},
-        {"Max Mana: ", currentCrest.m_max_mana},
-        {"Health Regeneration: ", currentCrest.m_health_regeneration},
-        {"Ability Haste: ", currentCrest.m_ability_haste},
-        {"Movement Speed: ", currentCrest.m_movement_speed}
+        {"Max Health", currentCrest.m_max_health},
+        {"Max Mana", currentCrest.m_max_mana},
+        {"Health Regeneration", currentCrest.m_health_regeneration},
+        {"Ability Haste", currentCrest.m_ability_haste},
+        {"Movement Speed", currentCrest.m_movement_speed}
     };
 
     int validRowCount = 0;
@@ -53,8 +58,26 @@ void CrestPage::fillTable(){
         if(buff.second.isNull() || buff.second.toDouble() == 0){
             continue; //Skip if empty or zero value buffs
         }
-        ui->BuffTable->setItem(row,0,new QTableWidgetItem(buff.first));
-        ui->BuffTable->setItem(row,1,new QTableWidgetItem(buff.second.toString()));
+        //Allow for table to blend in with background
+        ui->BuffTable->setStyleSheet("QTableWidget {font-size:14px;background-color:transparent; border:none}");
+        //Setting items
+        ui->BuffTable->setItem(row,1,new QTableWidgetItem(buff.first));
+        ui->BuffTable->setItem(row,0,new QTableWidgetItem("+" + buff.second.toString()));
+
+        //Editing row attributes
+        QTableWidgetItem *item = ui->BuffTable->item(row,0);
+        if(!item){
+            item = new QTableWidgetItem();
+            ui->BuffTable->setItem(row,0,item);
+        }
+        QTableWidgetItem *text = ui->BuffTable->item(row,1);
+        if(!item){
+            text = new QTableWidgetItem();
+            ui->BuffTable->setItem(row,1,text);
+        }
+        item->setForeground(QColor("#69A656"));
+        text->setForeground(QColor("#868186"));
+
         row++;
     }
 
